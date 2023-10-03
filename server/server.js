be8c249b7dbc540c644b76e11ad9fb5596478ccb
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-10-02 18:48:59 -0600

    packages

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //creates an endpoint for the route /api
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.json({ message: 'Hello from ExpressJS' });
 });
+
+
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
