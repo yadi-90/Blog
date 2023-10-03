5a2bbbf2d3e31d14371f21174d87a5569b448df0
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-10-03 15:57:55 -0600

    added style to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Home from './components/home';
+
 import Form from './components/Form';
 import Post from './components/Post';
 import BlogPosts from './components/Blogpost'
@@ -8,16 +11,34 @@ function App() {
 
 
   return (
-    <>
-
+    <><Router>
+    <div className="App">
+<div className="content">
   <h1> Feeling Awesome!</h1>
-  <nav>
+  <nav> 
+     <a href="/">Home</a>
     <a href="/form"> Form</a>
-    <a href="blogpost">BlogPosts</a>
+    <a href="/BlogPosts">BlogPosts</a>
+  
   </nav>
-
-<Home />
-<BlogPosts />
+{/* need to add routes  */}
+<Routes>
+  <Route path="/" element={<Home />} />
+</Routes>
+<Routes>
+  <Route path="/Form" element={<Form />} />
+</Routes>
+<Routes>
+  <Route path="/BlogPosts" element={<BlogPosts />} />
+</Routes>
+<Routes>
+  <Route path="/BlogPosts/:id" element={<Post />} />
+</Routes>
+</div>
+</div>
+</Router><div>
+  <Post />
+</div>
     </>
   )
 }
